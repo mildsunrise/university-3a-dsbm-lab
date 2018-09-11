7044e41f4fd35aea546913ecafade3872e339ddb
Alba Mendez <me@jmendeth.com>
2018-09-11 17:20:53 +0200

    Initial import from provided file
    Converted from LATIN1 / CRLF to standard stuff.
    
    for i in *; iconv -f LATIN1 -t UTF-8 "$i" > "$i.t"; and mv "$i.t" "$i"; end
    dos2unix *

---
+++ main.c
@@ -0,0 +1,43 @@
+/*************************************************************
+
+   m a i n . c
+
+   Practica 1
+
+ *************************************************************/
+
+#include "Base.h"     // Basic definitions
+
+// Function that blinks the green LED
+void ledBlink(void)
+ {
+ while(1)
+   {
+   // Turn on the green LED using the BSRR register
+   (LEDS_PORT->BSRR.H.set)=BIT(GREEN_LED_PAD);
+
+   // Wait 200ms
+   SLEEP_MS(200);
+
+   // Turn off the green LED using the BSRR register
+   (LEDS_PORT->BSRR.H.clear)=BIT(GREEN_LED_PAD);
+
+   // Wait 200ms
+   SLEEP_MS(200);
+   }
+ }
+
+int main(void)
+ {
+ // Basic initializations
+ baseInit();
+
+ // Call the LED blink function
+ // This function never returns
+ ledBlink();
+
+ // Return so that the compiler doesn't complain
+ // It is not really needed as ledBlink never returns
+ return 0;
+ }
+
