6e340f21f814978270d1123ea480ff4da09e6f60
Alba Mendez <alba.mendez@basetis.com>
2018-09-19 13:36:35 +0200

    P1: Implement and call ledSequence

--- main.c
+++ main.c
@@ -26,13 +26,32 @@ void ledBlink(void) {
     }
 }
 
+#define TURN_ON(bits) (LEDS_PORT->BSRR.H.set) = (bits);
+#define TURN_OFF(bits) (LEDS_PORT->BSRR.H.clear) = (bits);
+
+#define TURN_ON_DURING(time, bits) \
+        TURN_ON(bits); SLEEP_MS(time); TURN_OFF(bits);
+
+void ledSequence(void) {
+    while (1) {
+        TURN_ON_DURING(500, GREEN_LED_BIT);
+        TURN_ON_DURING(500, ORANGE_LED_BIT);
+        TURN_ON_DURING(500, RED_LED_BIT);
+        TURN_ON_DURING(500, BLUE_LED_BIT);
+    }
+}
+
 int main(void) {
     // Basic initializations
     baseInit();
 
     // Call the LED blink function
     // This function never returns
-    ledBlink();
+    //ledBlink();
+
+    // Call the LED sequence function
+    // This function never returns
+    ledSequence();
 
     // Return so that the compiler doesn't complain
     // It is not really needed as ledBlink never returns
